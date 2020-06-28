a5d0eb46c88210d468d766ff10abd053adeb73bf
Social Codia <socialcodia@gmail.com>
2020-06-28 13:42:34 +0530

    Added ForgotPasswordActivity

---
+++ app/src/main/java/com/socialcodia/socialui/activity/ForgotPasswordActivity.java
@@ -0,0 +1,16 @@
+package com.socialcodia.socialui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.socialcodia.socialui.R;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
