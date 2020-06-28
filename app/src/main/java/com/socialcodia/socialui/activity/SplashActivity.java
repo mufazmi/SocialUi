e4ea30425409ce3e3f5d9460ac7cde7877fac484
Social Codia <socialcodia@gmail.com>
2020-06-28 15:03:04 +0530

    Completed SplashActivity Code

--- app/src/main/java/com/socialcodia/socialui/activity/SplashActivity.java
+++ app/src/main/java/com/socialcodia/socialui/activity/SplashActivity.java
@@ -2,7 +2,9 @@ package com.socialcodia.socialui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.socialcodia.socialui.R;
 
@@ -12,5 +14,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sendToLogin();
+            }
+        },1000);
+    }
+
+    private void sendToLogin()
+    {
+        Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
     }
 }
