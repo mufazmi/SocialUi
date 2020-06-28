11a6eda219204bb023daed8d456d40481e9c7070
Social Codia <socialcodia@gmail.com>
2020-06-28 13:39:12 +0530

    Login Activity has been designed by @mufazmi

--- app/src/main/java/com/socialcodia/socialui/activity/LoginActivity.java
+++ app/src/main/java/com/socialcodia/socialui/activity/LoginActivity.java
@@ -1,13 +1,16 @@
-package com.socialcodia.socialui;
+package com.socialcodia.socialui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.socialcodia.socialui.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
--- app/src/main/java/com/socialcodia/socialui/activity/MainActivity.java
+++ app/src/main/java/com/socialcodia/socialui/activity/MainActivity.java
@@ -1,14 +1,15 @@
-package com.socialcodia.socialui;
+package com.socialcodia.socialui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+import com.socialcodia.socialui.R;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
